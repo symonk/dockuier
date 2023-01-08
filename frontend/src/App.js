d8d5926c3ff9b7536439c140c067c863fa206a5c
symonk <jackofspaces@gmail.com>
2023-01-08 18:11:29 +0000

    adding `material-ui` as a frontend dependency

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/header';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Header/>
       </header>
     </div>
   );
